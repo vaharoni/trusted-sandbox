0b7ee9ded911c61c1a673f2b258e0b3ad3ea801b
Amit Aharoni <amit.sites@gmail.com>
2014-10-29 17:11:13 -0700

    Removed keyword arguments

--- lib/trusted_sandbox/uid_pool.rb
+++ lib/trusted_sandbox/uid_pool.rb
@@ -34,16 +34,16 @@ module TrustedSandbox
     # @option timeout [Integer] number of seconds to wait for the lock
     # @option retries [Integer] number of attempts to retry to acquire a uid
     # @option delay [Float] delay between retries
-    def initialize(lock_dir, lower, upper, timeout: nil, retries: nil, delay: nil)
+    def initialize(lock_dir, lower, upper, options={})
       @lock_dir = lock_dir
       FileUtils.mkdir_p(lock_dir)
 
       @master_lock_file = lock_file_path_for('master')
       @lower = lower
       @upper = upper
-      @timeout = timeout || 3
-      @retries = retries || 5
-      @delay = delay || 0.5
+      @timeout = options[:timeout] || options['timeout'] || 3
+      @retries = options[:retries] || options['retries'] || 5
+      @delay = options[:delay] || options['delay'] || 0.5
     end
 
     def inspect
